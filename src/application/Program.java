e45a10978ec25b7fd4c2e6e4e6c49d7941826a47
carlosafer <carlosafdev@gmail.com>
2023-06-08 21:47:49 -0400

    Repository created

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		int a = 4;
+		int b = 9;
+		int c = 15;
+
+		int sum = a + b + c;
+		System.out.println(sum);
+	}
+
+}
